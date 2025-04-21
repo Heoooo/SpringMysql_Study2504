ac625b62bee86e9731a54f4d18e828b7b205add5
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-04-21 10:12:40 +0900

    0421:MySQL 기반의 Spring Security 프레임워크 연동1 - 강의개요

---
+++ MysqlSecurity/src/test/java/com/example/mysqlsecurity/MysqlSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqlsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
