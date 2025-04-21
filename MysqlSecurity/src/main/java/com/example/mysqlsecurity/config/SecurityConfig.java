078a7585b5b2f282d351492a9c30716c7ea19c57
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-04-21 14:11:14 +0900

    0421:MySQL 기반의 Spring Security 프레임워크 연동4 - SecurityConfig 설정

---
+++ MysqlSecurity/src/main/java/com/example/mysqlsecurity/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.mysqlsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration //스프링 환경설정 파일임을 의미
+@EnableWebSecurity //모든 요청 URL이 스프링 시큐리티 프레임워크의 통제를 받아라~ 라고 하는 애너테이션
+@EnableMethodSecurity(prePostEnabled = true) //@PreAuthorize() 활성화 애너테이션
+public class SecurityConfig {
+	
+	//빈으로 등록하는 메서드 => filterchain(), bCryptPasswordEncoder()
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		//람다식 사용하여 Spring Security 인가 설정 시작
+		//authorizeHttpRequests 메소드를 통해 권한 설정을 위한 객체 생성하고, 람다식을 사용하여 여러가지 설정 내용 정의
+		//authorizeHttpRequests 메서드 역할
+		//1. 이 메서드는 스프링 시큐리티 구성 메서드 내에서 사용되는 메서드 => HTTP 요청에 대한 인가 설정을 구성할 때 사용
+		//2. 따라서, 이놈을 사용해서 다양한 인가 규칙 및 각각의 페이지 경로별로 다른 권한을 설정 => 람다식 사용 => requestMatchers() 사용
+		http
+			.authorizeHttpRequests((auth)->auth
+					.requestMatchers("/").permitAll()
+					.requestMatchers("/login", "/signup").permitAll()
+					.requestMatchers("/admin").hasRole("ADMIN")
+					.anyRequest().authenticated()
+			);
+		
+		
+		return http.build();
+	}
+	
+}
